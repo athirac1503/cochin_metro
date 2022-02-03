e9adfa553eab190e2ad541f81a562083f26a3c7f
athirac1503 <athirac1503@gmail.com>
2022-02-03 14:17:14 +0530

    index page edited

--- metro/urls.py
+++ metro/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from. import views
 urlpatterns=[
-    path('index/',views.fnIndex,name='index/')
+    path('index/',views.fnIndex,name='index/'),
+    path('home/',views.fnHome,name='home/')
 ]
--- metro/views.py
+++ metro/views.py
@@ -3,3 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 def fnIndex(request):
     return render(request,'index.html')
+def fnHome(req):
+    return render(req,'home.html')
