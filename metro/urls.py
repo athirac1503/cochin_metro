9cfc53592c7e9084d8366aa8af1554c71ff90ca3
athirac1503 <athirac1503@gmail.com>
2022-02-24 20:18:22 +0530

    version change

--- metro/urls.py
+++ metro/urls.py
@@ -2,6 +2,13 @@ from django.urls import path
 from. import views
 urlpatterns=[
     path('login',views.fnLogin,name='login'),
-    path('home/',views.fnHome,name='home/'),
-    path('signup/',views.fnSignup,name='signup/')
+    path('home',views.fnHome,name='home'),
+    path('signup',views.fnSignup,name='signup'),
+    path('station',views.fnStation,name='station'),
+    path('index',views.fnIndex,name='index'),
+    path('ticket',views.fnTicket,name='ticket'),
+    path('contact',views.fnContact,name='contact'),
+    path('contact1',views.fnContact1,name='contact1'),
+    path('about',views.fnAbout,name='about'),
+    path('price',views.fnPrice,name='price')
 ]
--- metro/views.py
+++ metro/views.py
@@ -3,8 +3,24 @@ from django.shortcuts import render
 # Create your views here.
 def fnLogin(request):
     return render(request,'login.html')
-def fnHome(req):
-    return render(req,'home.html')
-def fnSignup(requ):
-    return render(requ,'signup.html')
+def fnHome(request1):
+    return render(request1,'home.html')
+def fnSignup(request2):
+    return render(request2,'signup.html')
+def fnStation(request3):
+    return render(request3,'station.html')
+def fnIndex(request4):
+    return render(request4,'index.html')
+def fnTicket(request5):
+    return render(request5,'ticket.html')
+def fnContact(request6):
+    return render(request6,'contact.html')
+def fnContact1(request7):
+    return render(request7,'contact1.html')
+def fnAbout(request8):
+    return render(request8,'about.html')
+def fnPrice(request9):
+    return render(request9,'price.html')
+
+
 
